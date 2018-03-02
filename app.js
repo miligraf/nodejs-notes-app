5e8848e6ba4417995474378e231c30e1b263f6a9
Milton Andrade <miligraf@gmail.com>
2018-03-02 14:40:29 -0500

    If statements; Use yargs to extract values from arguments

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('true'));
+const argv = yargs.argv;
+var command = argv._[0];
 
-var filteredArray = _.uniq([1,1,2,2,3,3,4,4,5,56]);
-console.log(filteredArray);
-
-// var res = notes.addNote();
-// console.log(res);
-
-// console.log(notes.add(-2, 9));
-
-// var user = os.userInfo();
-//
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+if (command ==='add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  notes.getNote(argv.title);
+} else if (command === 'remove') {
+  notes.removeNote(argv.title);
+} else {
+  console.log('Command not recognized');
+}
